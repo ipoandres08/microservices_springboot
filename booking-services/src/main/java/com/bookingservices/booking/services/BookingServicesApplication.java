7984b15d2bc33e122ae5a51d2b024dccdbffe665
ipoandres08 <andresiporre07@gmail.com>
2022-08-02 17:23:31 -0400

    Initial Services configurations

---
+++ booking-services/src/main/java/com/bookingservices/booking/services/BookingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bookingservices.booking.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServicesApplication.class, args);
+	}
+
+}
